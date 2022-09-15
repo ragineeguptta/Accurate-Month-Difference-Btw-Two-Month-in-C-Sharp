410db1c5488fedff88d94f1352a750d0fc7d22f2
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-09-15 18:48:19 +0530

    1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace RenewalMonth
         static void Main(string[] args)
         {
 
-            DateTime startDate = new DateTime(2021, 08, 01);
+            DateTime startDate = new DateTime(2021, 08, 05);
             DateTime renewalDate = startDate.AddMonths(12);
             DateTime currentDate = DateTime.Now;
 
@@ -23,12 +23,15 @@ namespace RenewalMonth
             Console.WriteLine("remianingPeriods : {0}", remianingPeriods);
 
 
-
+            //Formula for get extra remainingDaysOfMonth in decimal form
             double remainingDaysOfMonth = (remainingDays - (double)(remianingPeriods * daysOfMonth)) / (double)daysOfMonth;
             Console.WriteLine("remainingDaysOfMonth : {0}", remainingDaysOfMonth);
 
-
+            // adding whole and decimal months
             double remainingMonthInDouble = remainingDaysOfMonth + remianingPeriods;
+
+            // for two decimal point
+            remainingMonthInDouble = Math.Round(remainingMonthInDouble, 2);
 
             Console.WriteLine("remainingMonthInDouble : {0}", remainingMonthInDouble);
 
